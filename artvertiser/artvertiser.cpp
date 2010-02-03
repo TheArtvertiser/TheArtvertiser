9d66d948da01811d6128865b14dd05163cd6d4f3
Julian Oliver <julian@julianoliver.com>
2010-02-03 07:37:30 +0100

    modified:   artvertiser/artvertiser.cpp

--- artvertiser/artvertiser.cpp
+++ artvertiser/artvertiser.cpp
@@ -45,6 +45,20 @@
  *
  */
 
+#include "multigrab.h"
+
+// read from arduino
+#include <stdio.h>    /* Standard input/output definitions */
+#include <stdlib.h> 
+#include <stdint.h>   /* Standard types */
+#include <string.h>   /* String function definitions */
+#include <unistd.h>   /* UNIX standard function definitions */
+#include <errno.h>    /* Error number definitions */
+#include <fcntl.h>    /* File control definitions */
+#include <errno.h>    /* Error number definitions */
+#include <fcntl.h>    /* File control definitions */
+#include <termios.h>  /* POSIX terminal control definitions */
+
 #include <iostream>
 #include <sstream> // for conv int->str
 #include <vector>
@@ -55,13 +69,11 @@
 #include <stdio.h>
 #include <time.h>
 
-
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
 
 #include <calib/camera.h>
-#include "multigrab.h"
 
 #ifdef HAVE_APPLE_OPENGL_FRAMEWORK
 #include <GLUT/glut.h>
@@ -97,6 +109,16 @@ using namespace std;
 
 #define NUMARTVERTS 5
 
+// buttons via arduino
+int serialport_init(const char* serialport, int baud);
+int serialport_read_until(int fd, char* buf, char until);
+
+// arduino vars
+int fd = 0;
+char serialport[256];
+int baudrate = B9600;  // default
+char buf[256];
+int rc,n;
 
 // we continue tracking for 1 second, then fade for 3
 static const float SECONDS_LOST_TRACK = 0.5f;
@@ -240,6 +262,9 @@ string status_string = "";
    return suffix;
    }
  */
+
+
+
 string getSettingsString()
 {
     planar_object_recognizer &detector(multi->cams[current_cam]->detector);
@@ -425,6 +450,89 @@ void exit_handler()
         delete multi;
     }
 }
+
+int serialport_init(const char* serialport, int baud)
+{
+    struct termios toptions;
+    int fd;
+    
+    //fprintf(stderr,"init_serialport: opening port %s @ %d bps\n",
+    //        serialport,baud);
+
+    fd = open(serialport, O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd == -1)  {
+        perror("init_serialport: Unable to open port ");
+        return -1;
+    }
+    
+    if (tcgetattr(fd, &toptions) < 0) {
+        perror("init_serialport: Couldn't get term attributes");
+        return -1;
+    }
+    speed_t brate = baud; // let you override switch below if needed
+    switch(baud) {
+    case 4800:   brate=B4800;   break;
+    case 9600:   brate=B9600;   break;
+#ifdef B14400
+    case 14400:  brate=B14400;  break;
+#endif
+    case 19200:  brate=B19200;  break;
+#ifdef B28800
+    case 28800:  brate=B28800;  break;
+#endif
+    case 38400:  brate=B38400;  break;
+    case 57600:  brate=B57600;  break;
+    case 115200: brate=B115200; break;
+    }
+    cfsetispeed(&toptions, brate);
+    cfsetospeed(&toptions, brate);
+
+    // 8N1
+    toptions.c_cflag &= ~PARENB;
+    toptions.c_cflag &= ~CSTOPB;
+    toptions.c_cflag &= ~CSIZE;
+    toptions.c_cflag |= CS8;
+    // no flow control
+    toptions.c_cflag &= ~CRTSCTS;
+
+    toptions.c_cflag |= CREAD | CLOCAL;  // turn on READ & ignore ctrl lines
+    toptions.c_iflag &= ~(IXON | IXOFF | IXANY); // turn off s/w flow ctrl
+
+    toptions.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // make raw
+    toptions.c_oflag &= ~OPOST; // make raw
+
+    // see: http://unixwiz.net/techtips/termios-vmin-vtime.html
+    toptions.c_cc[VMIN]  = 0;
+    toptions.c_cc[VTIME] = 20;
+    
+    if( tcsetattr(fd, TCSANOW, &toptions) < 0) {
+        perror("init_serialport: Couldn't set term attributes");
+        return -1;
+    }
+
+    return fd;
+}
+
+
+// arduino serial port read
+int serialport_read_until(int fd, char* buf, char until)
+{
+	char b[1];
+	int i=0;
+	do { 
+		int n = read(fd, b, 1);  // read a char at a time
+		if( n==-1) return -1;    // couldn't read
+		if( n==0 ) {
+			usleep( 10 * 1000 ); // wait 10 msec try again
+			continue;
+		}
+		buf[i] = b[0]; i++;
+	} while( b[0] != until );
+
+	buf[i] = 0;  // null terminate the string
+	return 0;
+}
+
 
 bool loadOrTrain( bool wants_training, const char* model_file )
 {
@@ -1313,6 +1421,8 @@ static void drawAugmentation()
         drawBackground(tex);
         */
 
+		serialport_read_until(fd, buf, '\n');
+		printf("read: %s\n",buf);
 
 
 #ifndef DEBUG_SHADER
