59a574a752bed221d12af2d0c24c17f5acbee823
artvertiser-binoculars <artvertiser@artvertiser.(none)>
2010-03-09 17:47:12 +0100

    for arco stuff
    	modified:   models.xml
    	modified:   start.sh

--- artvertiser/start.sh
+++ artvertiser/start.sh
@@ -4,9 +4,14 @@
 touch start.sh.started
 
 # avoid shell interference (see info coreutils 'sleep invocation')
-echo start.sh sleeping 5 seconds
+echo start.sh sleeping 10 seconds
 env sleep 5
-/home/artvertiser/Desktop/working/The-Artvertiser/artvertiser/artvertiser -vd 0 -vs 640 480 -ml models.xml 2>&1 /home/artvertiser/Desktop/working/The-Artvertiser/artvertiser/artvertiser_via_start.sh.log
+echo 5
+env sleep 5
+echo sleeped
+echo starting artvertiser...
+/home/artvertiser/Desktop/working/The-Artvertiser/artvertiser/artvertiser -vd 0 -vs 640 480 -ml models.xml -binoc 2>&1 | tee /home/artvertiser/Desktop/working/The-Artvertiser/artvertiser/artvertiser_via_start.sh.log
+
 
 # say we finished
 touch start.sh.finished
